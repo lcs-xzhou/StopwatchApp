c0d9ec9925cf95e59f31c16645296c01c694009a
Xinlin Zhou <XZhou@lcs.on.ca>
2023-10-30 14:30:51 -0400

    Initial Commit

---
+++ StopwatchApp/StopwatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchAppApp.swift
+//  StopwatchApp
+//
+//  Created by Fiona ZHOU on 2023-10-30.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
