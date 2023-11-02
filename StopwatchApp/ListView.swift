534383cd084f5e3b74825ece4bc4bb29844b68af
Xinlin Zhou <XZhou@lcs.on.ca>
2023-11-02 09:11:03 -0400

    Make the step of put the list into the design and valued the color, but size is still needed.

---
+++ StopwatchApp/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  StopwatchApp
+//
+//  Created by Fiona ZHOU on 2023-10-31.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
